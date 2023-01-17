52a8294c6d4bd41b5a81850690fe4cfbdd582999
iamhi <inpactt@gmail.com>
2023-01-17 03:52:14 +0100

    Adding https to swagger

--- config/src/main/java/com/github/iamhi/hizone/terreplein/v2/config/SwaggerConfig.java
+++ config/src/main/java/com/github/iamhi/hizone/terreplein/v2/config/SwaggerConfig.java
@@ -4,12 +4,23 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
 import static com.github.iamhi.hizone.terreplein.v2.config.SwaggerConfig.SECURITY_SCHEME_NAME;
 
 @Configuration
-@OpenAPIDefinition(info = @Info(title = "My API", version = "v1"))
+@OpenAPIDefinition(info = @Info(title = "Hi-Zone Terreplein-v2 api", version = "v1"),
+    servers = {
+        @Server(
+            url = "https://api.ibeenhi.com/hi-zone-api/terreplein-v2",
+            description = "Https variant"
+        ),
+        @Server(
+            url = "http://api.ibeenhi.com/hi-zone-api/terreplein-v2",
+            description = "Http variant"
+        )
+    })
 @SecurityScheme(
     name = SECURITY_SCHEME_NAME,
     type = SecuritySchemeType.HTTP,
