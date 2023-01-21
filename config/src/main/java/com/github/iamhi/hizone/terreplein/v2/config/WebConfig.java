4d2a0b0e72bb1857eda515d2ba17242900850152
iamhi <inpactt@gmail.com>
2023-01-21 01:12:09 +0100

    WebConfig cleanup

--- config/src/main/java/com/github/iamhi/hizone/terreplein/v2/config/WebConfig.java
+++ config/src/main/java/com/github/iamhi/hizone/terreplein/v2/config/WebConfig.java
@@ -4,35 +4,11 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig {
-
-//    @Bean
-//    CorsFilter corsWebFilter() {
-//        CorsConfiguration corsConfig = new CorsConfiguration();
-//        corsConfig.addAllowedOriginPattern("*");
-//        corsConfig.setMaxAge(8000L);
-//        corsConfig.addAllowedMethod(HttpMethod.GET);
-//        corsConfig.addAllowedMethod(HttpMethod.POST);
-//        corsConfig.addAllowedMethod(HttpMethod.DELETE);
-//        corsConfig.addAllowedMethod(HttpMethod.PUT);
-//        corsConfig.addAllowedHeader("Content-Type");
-//        corsConfig.addAllowedHeader("Accept");
-//        corsConfig.addAllowedHeader("User-Agent");
-//        corsConfig.setAllowCredentials(true);
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//
-//        source.registerCorsConfiguration("/**", corsConfig);
-//
-//        return new CorsFilter(source);
-//    }
 
     @Bean
     WebMvcConfigurer corsConfigurer() {
